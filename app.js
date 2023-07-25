3bd688c88809faf9cf5ce0e07024598c0c7f43a7
Riha Hart <nathriha@gmail.com>
2023-07-25 19:21:05 -0400

    Website Draft 1
    Need to make it more responsive.

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("THIS IS CHECK")
+
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email"); 
+const inputMessage = document.getElementById("message"); 
+const myForm = document.getElementById("myForm");
+const submit = document.getElementById('submitButton');
+let successMessage = document.getElementById("successMessage");
+
+
+
+submit.addEventListener("click", function (eve){
+   
+
+    if (inputName.value !== "" && inputEmail.value !== "" && inputMessage.value !== "") {
+        successMessage.innerText = "Riha has received your message, speak soon!";
+    } else {
+        successMessage.innerText = "Looks like you forgot something!";
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
