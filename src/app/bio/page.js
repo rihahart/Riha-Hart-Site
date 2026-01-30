eb368d65da088e9a976a4862fee5b77d982b5628
everestitc <support@everestfcu.org>
2026-01-30 18:45:35 -0500

    new type

--- src/app/bio/page.js
+++ src/app/bio/page.js
@@ -15,7 +15,7 @@ export default function Bio() {
       <div className="w-full flex flex-col items-center px-[var(--spacing-lg)] pt-[var(--spacing-lg)] pb-[var(--spacing-8xl)] gap-[var(--spacing-xl)]">
         <div className="w-full flex flex-col items-center justify-center gap-[var(--spacing-2xl)]">
           <img src={bioData.image.src} alt={bioData.image.alt} />
-          <div className="flex flex-col items-start justify-center gap-[var(--spacing-xl)]">
+          <div className="flex flex-col items-start justify-center gap-[var(--spacing-lg)]">
             <h2 className="h2 text-[var(--lightblack)]">
               {bioData.heading}
             </h2>
@@ -36,7 +36,7 @@ export default function Bio() {
       <div className="flex flex-col items-center px-[var(--spacing-2xl)] pt-[var(--spacing-2xl)] pb-[var(--spacing-10xl)] gap-[var(--spacing-4xl)] w-full  mx-auto">
         <div className="w-full flex flex-col items-center justify-center gap-[var(--spacing-4xl)]">
           <img src={bioData.image.src} alt={bioData.image.alt} />
-          <div className="flex flex-col items-start justify-center gap-[var(--spacing-2xl)]">
+          <div className="flex flex-col items-start justify-center gap-[var(--spacing-xl)]">
             <h2 className="h2 text-[var(--lightblack)]">
               {bioData.heading}
             </h2>
@@ -55,9 +55,9 @@ export default function Bio() {
   if (isDesktop1440px) {
     return (
       <div className="flex flex-col items-center px-[var(--spacing-2xl)] gap-[var(--spacing-4xl)] pt-[var(--spacing-2xl)] pb-[var(--spacing-10xl)] w-full mx-auto">
-        <div className="w-full flex items-center justify-center gap-[var(--spacing-5xl)] py-[var(--spacing-2xl)]">
+        <div className="w-full flex items-center justify-center gap-[var(--spacing-5xl)] py-[var(--spacing-xl)]">
           <img src={bioData.image.src} alt={bioData.image.alt} className="w-[50%]" />
-          <div className="w-[50%] flex flex-col items-start justify-center gap-[var(--spacing-3xl)]">
+          <div className="w-[50%] flex flex-col items-start justify-center gap-[var(--spacing-2xl)]">
             <h2 className="h2 text-[var(--lightblack)]">
               {bioData.heading}
             </h2>
