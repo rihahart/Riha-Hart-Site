a654249d6b1b698c0e7b210ab704eb10da93b825
everestitc <support@everestfcu.org>
2026-03-16 22:29:16 -0400

    EH Page

--- src/data/EH/ResearchDeck.js
+++ src/data/EH/ResearchDeck.js
@@ -1,10 +1,10 @@
 const BASE = "/EarthHero/WhitePresentation/"
 
 export const researchDeckSlides = [
-  `${BASE}05 Insufficient Call to Action.png`,
-  `${BASE}01 Research recommendation.png`,
-  `${BASE}02 Unclear & Insufficient.png`,
-  `${BASE}03 Action & Earthpoints.png`,
-  `${BASE}04 EarthPoints, recommendations.png`,
-  `${BASE}06 Additional research.png`,
+  `${BASE}01UnmotivateUsers.png`,
+  `${BASE}02DisconnedSystem.png`,
+  `${BASE}03SeparatedMetrics.png`,
+  `${BASE}04ConfusingEP.png`,
+  `${BASE}05UnclearCTAs.png`,
+  `${BASE}06ContentAudit.png`,
 ]
